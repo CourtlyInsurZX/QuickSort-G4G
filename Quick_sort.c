3f489592c70bc9610bda7781240fb83f026b99f9
mgtrjemilio <54515791+mgtrjemilio@users.noreply.github.com>
2019-11-06 17:03:04 -0500

    Update Quick_sort.c

--- Quick_sort.c
+++ Quick_sort.c
@@ -1,100 +1,64 @@
-#include <stdio.h>
 
-void print_ij(int i, int j, int size)
-{
-    int AuxIJ;
-    printf("\t");
-    for(AuxIJ = 0; AuxIJ < size; AuxIJ++)
-    {
-        if(AuxIJ== i && AuxIJ==j)
-        {
-            printf(" (ij)\t");
-        }
-        else if(AuxIJ == i)
-        {
-            printf(" (i)\t");
-        }
-        else if(AuxIJ == j)
-        {
-            printf(" (j)\t");
-        }
-        else
-        {
-            printf(" \t");
-        }
-    }
-    printf("\n");
-}
+#include<stdio.h> 
 
-void ImprimePivote(int i, int j, int size)
-{
-    int AuxPiv;
-    printf("\t");
-    for(AuxPiv = 0; AuxPiv < size; AuxPiv++)
-    {
-        if(AuxPiv == i)
-        {
-            printf("(i+1)\t");
-        }
-        else if(AuxPiv == j)
-        {
-            printf("(high)\t");
-        }
-        else
-        {
-            printf("\t");
-        }
-    }
-    printf("\n");
-}
-
-
-
-void quickSort(int arr[], int low, int high, int pivoteaux)
-{
-    if (low < high)
-    {  
-        int pi = partition(arr, low, high, pivoteaux);
-        quickSort(arr, low, pi - 1, pivoteaux);
-        quickSort(arr, pi + 1, high, pivoteaux);
-    }
-}
-
-void swap(int* a, int* b)
-{
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
-
-void printArray(int arr[])
-{
-    int n = sizeof(arr)-1;
-    int i;
-        for (i=0; i < n; i++)
-        {
-             printf("|%d|\t", arr[i]);
-        }
-    printf("\n\n");
+void swap(int* a, int* b) 
+{ 
+	int t = *a; 
+	*a = *b; 
+	*b = t; 
 } 
 
 
+int partition (int arr[], int low, int high) 
+{ 
+	int pivot = arr[high]; 
+	int i = (low - 1); 
 
-// ----- FUNCIÓN PRINCIPAL ----- 
-int main()
-{
-    int arr[] = {10, 80, 30, 90, 40, 50, 70};
-    int n = (sizeof(arr)/sizeof(arr[0])) - 1;
-    int pivoteaux = arr[n];
-    printf("\t\tARREGLO DESORDENADO:\n\n\t");
-    
-    printArray(arr);
-    printf("----------------------------Quick Sort----------------------------\n\n");
-    quickSort(arr, 0, n, pivoteaux);
-    printf("\t\tARREGLO ORDENADO:\n\n\t");
-    printArray(arr);
-    
-    
-    return 0;
-}
+	for (int j = low; j <= high- 1; j++) 
+	{ 
+		/
+		if (arr[j] < pivot) 
+		{ 
+			i++; 
+			swap(&arr[i], &arr[j]); 
+		} 
+	} 
+	swap(&arr[i + 1], &arr[high]); 
+	return (i + 1); 
+} 
 
+
+arr[] --> Array to be sorted, 
+low --> Starting index, 
+high --> Ending index */
+void quickSort(int arr[], int low, int high) 
+{ 
+	if (low < high) 
+	{ 
+		
+		at right place */
+		int pi = partition(arr, low, high); 
+		quickSort(arr, low, pi - 1); 
+		quickSort(arr, pi + 1, high); 
+	} 
+} 
+
+
+void printArray(int arr[], int size) 
+{ 
+	int i; 
+	for (i=0; i < size; i++) 
+		printf("%d ", arr[i]); 
+	printf("n"); 
+} 
+
+
+int main() 
+{ 
+	int arr[] = {10, 7, 8, 9, 1, 5}; 
+	int n = sizeof(arr)/sizeof(arr[0]); 
+	quickSort(arr, 0, n-1); 
+	printf("Sorted array: n"); 
+	printArray(arr, n); 
+	return 0; 
+} 
